459c569675eec21a208e2ccc097ea555e8292dbd
andreburto <tuglyraisin@gmail.com>
2021-04-02 15:57:13 -0500

    Initial commit, works.

---
+++ app.py
@@ -0,0 +1,41 @@
+# Standard library
+import os
+import json
+
+# Third party
+from flask import Flask, redirect, make_response
+
+# Constants
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+PICS_PATH = f"{CURRENT_PATH}/pics"
+DISPLAY_FILE = "display.json"
+PYXL_FILE = "pyxl.html"
+DEFAULT_PIC = "red.json"
+
+# The main Flask app
+app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
+
+
+@app.route("/", methods=["GET"])
+def index():
+    with open(f"{app.static_folder}/{PYXL_FILE}", "r") as fp:
+        response = fp.readlines()
+        return str(response)
+
+
+@app.route("/image", methods=["GET"])
+@app.route("/image/<string:pic>", methods=["GET"])
+def image(pic=DEFAULT_PIC):
+    if not pic.lower().endswith(".json"):
+        file_name = f"{pic}.json"
+    else:
+        file_name = pic
+
+    with open(f"{PICS_PATH}/{file_name}", "r") as fp:
+        resp = make_response(json.load(fp))
+        resp.headers["Content-Type"] = "application/json"
+        return resp
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=False)
---
+++ static/pyxl.js
@@ -0,0 +1,83 @@
+const contentName = "content"
+const canvasName = "theCanvas"
+
+const getPixelSize = (canvas, count, gap) => {
+    let totalGap = (count * gap) + gap
+    let pixelSize = Math.floor((canvas.height - totalGap) / count)
+    return pixelSize
+}
+
+const drawPixels = (display, pixelCount, pixelGap) => {
+    let c = document.getElementById(canvasName)
+    let ctx = c.getContext("2d");
+    let pixelSize = getPixelSize(c, pixelCount, pixelGap)
+
+    let yPos = pixelGap
+
+    for(let cy = 0; cy < pixelCount; cy++) {
+        let xPos = pixelGap
+
+        for (let cx = 0; cx < pixelCount; cx++) {
+            ctx.fillStyle = display[cy][cx]
+            ctx.fillRect(xPos, yPos, pixelSize, pixelSize)
+            xPos += (pixelSize + pixelGap)
+        }
+
+        yPos += (pixelSize + pixelGap)
+    }
+}
+
+const loadImage = () => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            let resp = xhr.response
+            drawPixels(resp["display"], resp["count"], resp["gap"])
+        }
+        else {
+            console.log("Error code: " + xhr.status)
+        }
+    };
+
+    xhr.responseType = "json"
+    xhr.open("GET", "/image", true)
+    xhr.send();
+}
+
+const setCanvasSize = () => {
+    let c = document.getElementById(canvasName)
+    c.width = c.parentElement.offsetWidth
+    c.height = c.parentElement.offsetHeight
+}
+
+const setDivSize = () => {
+    let html = document.documentElement;
+    let divSize = Math.min(html.clientWidth, html.clientHeight)
+    let div = document.getElementById(contentName)
+    div.style.width = divSize + "px"
+    div.style.height = divSize + "px"
+}
+
+const centerDisplayDiv = () => {
+    let html = document.documentElement;
+    let div = document.getElementById(contentName)
+    // Calculate horizontal spacing.
+    let divX = Math.floor((html.clientWidth - div.offsetWidth) / 2)
+    // Calculate vertical spacing.
+    let divY = Math.floor((html.clientHeight - div.offsetHeight) / 2)
+    // Assign positions to the two div elements.
+    div.style.left = divX + "px"
+    div.style.top = divY + "px"
+}
+
+const resizeAll = () => {
+    setDivSize()
+    centerDisplayDiv()
+    setCanvasSize()
+    loadImage()
+}
+
+const startUp = () => {
+    resizeAll()
+}
